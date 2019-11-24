0315fcf9565e821c67f86b782d00dba10b5673e4
Carlos Gonzalez Betancort <carlos.gonzalez@couchbase.com>
2019-11-24 13:57:47 +0000

    trying to fix relative ajax callss

--- app/data.service.js
+++ app/data.service.js
@@ -4,13 +4,13 @@ angular
     .service('dataService', function ($http) {
         return {
             GetZones: function () {
-                return $http.get('/assets/data/zones.json');
+                return $http.get('assets/data/zones.json');
             },
             GetTeam: function () {
-                return $http.get('/assets/data/team.json');
+                return $http.get('assets/data/team.json');
             },
             GetUsers: function () {
-                return $http.get('/assets/data/users.json').then((resp) => {
+                return $http.get('assets/data/users.json').then((resp) => {
                     console.log("resp: ", resp, "data: ", resp.data);
                     return resp.data
                 }).catch((_) => {return {}});
